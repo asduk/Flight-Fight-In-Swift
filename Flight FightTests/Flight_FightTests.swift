4b3ef48fe84eb4a97921d6891e1b37b33b3b5276
asduk <yzm@yzmdeMacBook-Air.local>
2014-06-05 22:14:32 +0800

    Initial Commit

---
+++ Flight FightTests/Flight_FightTests.swift
@@ -0,0 +1,35 @@
+//
+//  Flight_FightTests.swift
+//  Flight FightTests
+//
+//  Created by asduk on 14-6-5.
+//  Copyright (c) 2014年 asduk. All rights reserved.
+//
+
+import XCTest
+
+class Flight_FightTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
